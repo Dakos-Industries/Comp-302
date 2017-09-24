72f8c3263f7dbbc84e323f918ce849a6103aeb67
Spiros <spirosmavroidakos@gmail.com>
2017-09-24 18:46:40 -0400

    Fixed formatting

--- hw1.ml
+++ hw1.ml
@@ -1,21 +1,21 @@
-(* Student information:
+(* student information:
 
-   Enter your name, and if you chose to work in pairs, the name of the
-   student you worked with (both students MUST submit the solution to
-   myCourses):
+   enter your name, and if you chose to work in pairs, the name of the
+   student you worked with (both students must submit the solution to
+   mycourses):
 
-   Name:	Spiros Mavroidakos
-   McGill ID:	260689391
+   name:	Spiros Mavroidakos
+   mcgill id:	260689391
 *)
 
-(* Homework 1 - Questions 2 and 3 *)
+(* homework 1 - questions 2 and 3 *)
 
-(* First, some utility functions and declarations that you can use. Be
-   sure to check Ocaml's documentation to find more functions
+(* first, some utility functions and declarations that you can use. be
+   sure to check ocaml's documentation to find more functions
    available to you.
 
-   You can start checking the documentation at:
-   https://caml.inria.fr/pub/docs/manual-ocaml/libref/Pervasives.html
+   you can start checking the documentation at:
+   https://caml.inria.fr/pub/docs/manual-ocaml/libref/pervasives.html
  *)
 
 (* the value of pi *)
@@ -35,20 +35,20 @@ let is_multiple_of (a : float) (b : float) : bool =
 (* a function to check if a is between plus/minus b *)
 let abs_btwn a b = a < b && a > ~-.b
 
-(* Question 2: Triangles are the best *)
+(* question 2: triangles are the best *)
 
 type side = float
 
 type tr_by_sides = side * side * side
 
 type tr_kind
-  = Scalene
-  | Equilateral
-  | Isosceles
+  = scalene
+  | equilateral
+  | isosceles
 
-(* Question 2.1 
-	The logic is simple, if one of the possible true values fails,
-	then false is returned. If they all pass then it is true as 
+(* question 2.1 
+	the logic is simple, if one of the possible true values fails,
+	then false is returned. if they all pass then it is true as 
 	explained in the qestion.
 *)
 let well_formed_by_sides (a, b, c : tr_by_sides) : bool =
@@ -60,31 +60,31 @@ let well_formed_by_sides (a, b, c : tr_by_sides) : bool =
 	else false
 
 
-(* Question 2.2 
-	Creates a triangle based on the area and type of triangle.
-	Cases:
-		1.Equilateral:
-			Basic formula to get the dimensions. Commonly known.
-		2. Isosceles:
+(* question 2.2 
+	creates a triangle based on the area and type of triangle.
+	cases:
+		1.equilateral:
+			basic formula to get the dimensions. commonly known.
+		2. isosceles:
 			ab is the length of the sides a and b since they are equal.
-			Since an Isosceles can be modeled as half of a square, ab 
-			can be viewed as the length of a side of the square. Knowing 
+			since an isosceles can be modeled as half of a square, ab 
+			can be viewed as the length of a side of the square. knowing 
 			that the area is the lenght of a side^2 for a square, you can
 			multiply the area of the triangle by 2 and then sqrt it to get 
-			the length. Using pythagorean theorem, you can then find the length 
+			the length. using pythagorean theorem, you can then find the length 
 			of the 3rd side c.
-		3. Scalene:
-			Asumptions: The base is always equal to the area and the height is
+		3. scalene:
+			asumptions: the base is always equal to the area and the height is
 			therefore always 2. 
-			Using these assumptions, if we partition the base so that it is
+			using these assumptions, if we partition the base so that it is
 			split anywhere that is not the middle, then the resulting triangle 
-			will be scalene. Here I choose to partition it at 1/3 of the length 
-			of the base. Using those assumptions, you can use pythagorean theorem
+			will be scalene. here i choose to partition it at 1/3 of the length 
+			of the base. using those assumptions, you can use pythagorean theorem
 			since you know 2 sides so you can find the 3rd.
 
 *)
 let create_triangle (kind : tr_kind) (area : float) : tr_by_sides = match kind with 
-  	| Equilateral -> let (sides:side) = sqrt((4.0 *. area) /. sqrt(3.0)) in 
+  	| equilateral -> let (sides:side) = sqrt((4.0 *. area) /. sqrt(3.0)) in 
   			(sides,sides,sides) 
   	| Isosceles -> let ab:float = sqrt(2.0 *. area) in
   			let c:side = sqrt(2.0 *. ab *. ab) in
@@ -133,14 +133,14 @@ let even (n : int) : bool = n mod 2 = 0
 	lists are combined and returned.
 *)
 let evens_first (l : int list) : int list =
- let rec even_rec l acc_even acc_odd = match l with
+	let rec even_rec l acc_even acc_odd = match l with
 	| [] -> acc_even@acc_odd
 	| h::t -> if (even(h)) then even_rec t (acc_even@[h]) acc_odd
 			else even_rec t acc_even (acc_odd@[h])
 	
 	in  
-  even_rec l [] []
-;;		 
+  	even_rec l [] [];;
+
 let ex_1 = evens_first [7 ; 5 ; 10; 12; 6; 3; 4; 2; 1]
 (* val ex_1 : int list = [2; 4; 6; 4; 2; 7; 5; 3; 1] *)
 
@@ -157,8 +157,8 @@ let even_streak (l : int list) : int =
 	| h::t -> if (even(h)) then streak t acc_old (acc_new + 1)
 		else if (acc_old > acc_new) then streak t acc_old 0
 			else streak t acc_new 0
- in 
- streak l 0 0
+ 	in 
+ 	streak l 0 0
 
 let ex_2 = even_streak [7; 2; 4; 6; 3; 4; 2; 1; 2; 2; 2; 2; 2]
 
@@ -169,13 +169,13 @@ let ex_2 = even_streak [7; 2; 4; 6; 3; 4; 2; 1; 2; 2; 2; 2; 2]
 type nucleobase = A | G | C | T
 
 let compress (l : nucleobase list) : (int * nucleobase) list =
-  let rec dna_magic l counter current nucleo_l = match l with
+	let rec dna_magic l counter current nucleo_l = match l with
   	|[] -> nucleo_l@[(counter, current)]
 	|h::t -> if (counter = 0) then dna_magic t 1 h nucleo_l
 		else if (current = h) then dna_magic t (counter + 1) current nucleo_l
 			else dna_magic t 1 h (nucleo_l@[(counter,current)])
-  in
-  dna_magic l 0 A []
+  	in
+  	dna_magic l 0 A []
 
 (*
 	Helper Method for decompress. Take in a counter which is the 
@@ -191,11 +191,11 @@ let rec decompress_printer counter nucleo decompress_l  =
 
 
 let rec decompress (l : (int * nucleobase) list) : nucleobase list =
-  let rec dna_helper l decomp_l = match l with
-  |[] -> decomp_l
-  |((s,k) as h)::t -> dna_helper t (decompress_printer s k decomp_l)
-  in
-  dna_helper l []
+	let rec dna_helper l decomp_l = match l with
+  	|[] -> decomp_l
+  	|((s,k) as h)::t -> dna_helper t (decompress_printer s k decomp_l)
+  	in
+  	dna_helper l []
 
 let sample_dna : nucleobase list = [A;A;A;A;G;G;A;T;T;T;C;T;C]
 
