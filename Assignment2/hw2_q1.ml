a15c798c1796a36863feaec50f765a83fa89b422
Spiros <spirosmavroidakos@gmail.com>
2017-09-28 19:06:06 -0400

    Assignment 2 initial commit

---
+++ Assignment2/hw2_q1.ml
@@ -0,0 +1,73 @@
+
+type prop = Atom of string
+          | Not of prop
+          | And of prop * prop
+          | Or of prop * prop
+
+let impl (p, q) = Or(Not p, q)
+let iff (p, q) = And (impl (p,q), impl (q, p))
+
+let mp = impl (And (Atom "P", impl (Atom "P", Atom "Q")), Atom "Q")
+
+(* Proof by contradiction (reduction ad absurdum): ((¬ P) ⇒ Q) ∧ ((¬ P) ⇒ (¬ Q)) ⇒ P *)
+let ra = impl (
+             And (impl (Not (Atom "P"),
+                        Atom "Q"),
+                  impl (Not (Atom "P"),
+                        Not (Atom "Q"))),
+             Atom "P")
+
+(* Atoms and their negations *)
+type signed_atom
+  = PosAtom of string
+  | NegAtom of string
+
+(* In NNF negations can only be applied to atoms, this datatype only
+   allows for propositions in NNF *)
+type nnf
+  = AndN of nnf * nnf
+  | OrN of nnf * nnf
+  | AtomN of signed_atom
+
+(* Q1.2: Write the function nnf that converts propositions into NNF,
+   notice that the typechecker will guide you in your answer as the
+   type forces the result to be in NNF. Your job is to not forget any
+   sub-parts of the proposition in the conversion. *)
+let rec to_nnf : prop -> nnf = function
+  | _ -> assert false
+
+
+(* Q1.3: Write a datatype cnf that represents only propositions in
+   cnf. Hint: You might want to use more than one type to be able to
+   represent sub-expressions.*)
+
+(* type cnf =  *)
+
+(* Q1.4: Write the distribute and nnf_to_cnf functions using the new
+   datatype. Hint: you may need more than one helper function. *)
+let rec distribute : cnf * cnf -> cnf = function
+  | _ -> assert false
+
+let rec nnf_to_cnf : nnf -> cnf = function
+  | _ -> assert false
+
+let to_cnf (p :prop) : cnf = nnf_to_cnf (to_nnf p)
+
+(* Q1.5: Write the new positives and negative atoms function as in the
+   previous version *)
+let rec positives = function
+  | _ -> assert false
+
+let rec negatives = function
+  | _ -> assert false
+
+(* Fill in the code for the intersection function from Q1.1 *)
+let rec intersection l1 l2 = assert false
+
+(* Q1.6: Write the new cnf_tautology function *)
+let rec cnf_tautology : cnf -> bool = function
+  | _ -> assert false
+
+let taut (p : prop) : bool = cnf_tautology (to_cnf p)
+let unsat (p : prop) : bool = taut (Not p)
+let sat (p : prop) : bool = not (unsat p)
