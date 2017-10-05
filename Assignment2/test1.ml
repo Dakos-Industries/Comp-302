356093e64be54fcdc32e469cc9777a291f9e2891
Spiros <spirosmavroidakos@gmail.com>
2017-10-05 08:38:08 -0400

    Added unit tests

--- Assignment2/test1.ml
+++ Assignment2/test1.ml
@@ -32,16 +32,24 @@ let sample = Hw2_q1.OrN(Hw2_q1.AtomN (Hw2_q1.NegAtom "P"), Hw2_q1.AtomN (Hw2_q1.
 
 let simpleAtom = Hw2_q1.AtomN(Hw2_q1.PosAtom "A")
 
+let simpleAtomC = Hw2_q1.SubCnf (Hw2_q1.AtomC(Hw2_q1.PosAtom "A"))
+
 let test4 test_ctxt = assert_equal true (sample = Hw2_q1.to_nnf(taut1'));;
 
 let test5 test_ctxt = assert_equal true (simpleAtom = Hw2_q1.to_nnf(Hw2_q1.Atom "A"))
+
+let test6 test_ctxt = assert_equal true (Hw2_q1.cnf_tautology(Hw2_q1.to_cnf(taut1')))
+
+let test7 test_ctxt = assert_equal true (simpleAtomC = Hw2_q1.to_cnf(Hw2_q1.Atom "A"))
 
 let suite = "Suite">:::
 ["test1">:: test1;
  "test2">:: test2;
  "test3">:: test3;
  "test4">:: test4;
- "test5">:: test5;];;
+ "test5">:: test5;
+ "test6">:: test6;
+ "test7">:: test7;];;
 
 let ()=
 run_test_tt_main suite;;
